4e3b916a0ec6014ac989b34a7f2c0c52237cfde7
somya <somyasharma0488@gmail.com>
2025-04-28 20:51:02 +0530

    main

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            int X, Y;
+            
+            cin>>X>>Y;
+            
+            cout<<(2*X)*5<<endl;
+            
+            cout<<(2*X)*5*Y<<endl;
+            
+        }
+    }
