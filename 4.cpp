467efbba6e0e6ac33f8ba30f58e867201a42adae
somya <somyasharma0488@gmail.com>
2025-04-30 22:33:47 +0530

    main

---
+++ 4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main(){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    float pi=3.14;
+    
+
+    cout<<"Hello! Here the value of a is "<< a <<". The value of b is " << b <<endl;
+    cout<<"The value of pi is: "<<pi<<endl;
+    
+    // cout<<"Circumference of the circle is: "<<2*pi*a<<endl;
+    // cout<<"Area of the circle is: "<<pi*a*a<<endl;
+    // return 0;
+
+ }
