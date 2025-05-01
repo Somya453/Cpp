cfa25f695a891ceb97af2aad17871a814468e168
somya <somyasharma0488@gmail.com>
2025-05-01 12:20:26 +0530

    main

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main(){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    
+
+    cout<<"Hello! Here the value of a is "<< a <<". The value of b is " << b <<endl;
+    
+    cout<<"Area of the triangle is: "<<a*b<<endl;
+    return 0;
+
+ }
