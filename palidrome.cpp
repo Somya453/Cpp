b7afa4d012bd5961202cec70394e3620c9907caf
somya <somyasharma0488@gmail.com>
2025-07-03 07:48:43 +0530

    main

---
+++ palidrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int reverse = 0;
+
+    int dup=n;
+    while(n>0){
+
+        int digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+
+    if(reverse == dup){
+        cout << "Palindrome";
+    }else{
+        cout << "Not Palindrome";
+    }
+}
