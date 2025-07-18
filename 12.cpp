a6d9a0f874c7c94aa87d66902af3799b1850b063
somyaa <somyasharma0488@gmail.com>
2025-07-18 20:21:34 +0530

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -1,10 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    for (int i = 1; i <=10; i++)
-    {
-        cout<<i*6<<endl;
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 5 == 0 && num % 3 == 0) {
+        cout << "The number is divisible by both 5 and 3." << endl;
+    } else {
+        cout << "The number is not divisible by both 5 and 3." << endl;
     }
+
     
 }
