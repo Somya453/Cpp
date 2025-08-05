cb0e6c72b918eeb3a7899cc70dce868869d3533f
somya <somyasharma0488@gmail.com>
2025-08-05 08:56:36 +0530

    main

---
+++ averg.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    float a=2;
+    float b=3;
+    float c=4;
+    float d=5;
+
+    float result=(a+b+c+d)/4;
+    cout<<result;
+    
+    return 0;
+}
