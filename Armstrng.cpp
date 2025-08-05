20cfc7771e9d21ec4b14d7370087ca2c8550558a
somya <somyasharma0488@gmail.com>
2025-08-05 13:19:21 +0530

    main

--- Armstrng.cpp
+++ Armstrng.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
+#include <cmath> // for pow()
 using namespace std;
+
 // This code checks if a number is an Armstrong number or not.
-
-
 
 class Solution {
 public:
     bool isArmstrong(int n) {
-
         int sum = 0;
-        int num = 7;
-        int digit = 0;
+        int num = n;
+        int digits = 0;
 
-        // Count the number of digits
-        while (num > 0) {
-            digit=num % 10;
-            sum=sum+(digit*digit*digit);
-            num /= 10;
-            
+        // Count number of digits
+        int temp = n;
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
         }
 
-        if (sum == n) return  true;
-        else return false;
+        // Calculate sum of digits raised to power of total digits
+        temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += pow(digit, digits);
+            temp /= 10;
+        }
 
-
+        return (sum == n);
     }
 };
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    Solution obj;
+    if (obj.isArmstrong(number)) {
+        cout << "Armstrong Number" << endl;
+    } else {
+        cout << "Not an Armstrong Number" << endl;
+    }
+
+    return 0;
+}
