2e42a7e8d14ad3e8eaee82644536a93b17185a0f
somya <somyasharma0488@gmail.com>
2025-08-05 13:17:22 +0530

    main

--- index.cpp
+++ index.cpp
@@ -4,15 +4,32 @@ using namespace std;
 class Solution {
 public:
     bool isPrime(int n) {
-        //your code goes here
+        // 1 is not prime
+        if (n <= 1) return false;
 
-        // cin>>n;
-        if(n==1) return false;
-        for(int i=2; i<n; i++ ){
-            if(n%i==0){
+        // Check divisibility from 2 to sqrt(n) for efficiency
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
+
         return true;
     }
 };
+
+int main() {
+    int number;
+    cin >> number;  // Taking input
+
+    Solution obj;
+    bool result = obj.isPrime(number);  // Calling the isPrime function
+
+    // Output
+    if (result)
+        cout << "Prime" << endl;
+    else
+        cout << "Not Prime" << endl;
+
+    return 0;
+}
