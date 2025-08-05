58744d13f20464c4a09fba0dcbfa44a1e35cefc9
somya <somyasharma0488@gmail.com>
2025-08-05 13:37:30 +0530

    main

---
+++ ASCIImulti.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    char a, b;
+    cout << "Enter two characters: ";
+    cin>>a>>b;
+
+    int(ch); // Convert characters to integers
+    //if we want to multiply characters, we can convert them to their ASCII values
+    //**if we want to convert the intereger into characters, we will use "char(int)"  */
+
+    int result=a*b;
+    cout<<result;
+
+    return 0;
+}
