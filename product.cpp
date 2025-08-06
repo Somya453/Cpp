3727c346ab169bbb18ac127689ac2dc37cef91fe
somya <somyasharma0488@gmail.com>
2025-08-06 19:49:43 +0530

    main

---
+++ product.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	long long int A, B, C;
+    cout << "Enter three integers: ";
+    // Taking input for three integers
+    cin>>A>>B>>C;
+
+   
+
+    long long int result=A*B*C;
+    cout<<result;
+
+    
+}
