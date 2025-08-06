c25d071c648d3323865f923b9ddf73ab5a1afbb3
somya <somyasharma0488@gmail.com>
2025-08-06 22:47:57 +0530

    main

--- SetPrecision.cpp
+++ SetPrecision.cpp
@@ -1,10 +1,8 @@
 
-
-
 // // 🔧 Why use setprecision(1)?
 
 // The problem says to show exactly one decimal place.
-
+//It helps in printing floating-point numbers with a specific number of digits after the decimal point.
 // It's a formatting tool to make your output match the required format exactly, which is very important in coding problems and tests.
 
 
@@ -16,7 +14,7 @@ using namespace std;
 
 void fahrenheit_to_celsius(float n) {
     float tempInCelsius = ((n - 32.0) * 5.0) / 9.0;
-    cout << fixed << setprecision(1) << tempInCelsius << endl;
+    cout << fixed << setprecision(2) << tempInCelsius << endl;
 }
 
 
